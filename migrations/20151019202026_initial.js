2962dd2811ceabc32296da115039ed0360c1935d
Mikko Välimäki <mikko.valimaki@futurice.com>
2015-12-27 10:23:45 +0200

    Random improvements, clean up
    - Add constraints for dataset unique fields
    - Hide database dates from some parts of the API
    - Add "drop_off_time" for stop_time table (used by the Google's example)
    - Split the long lines in the migration script

--- migrations/20151019202026_initial.js
+++ migrations/20151019202026_initial.js
@@ -5,38 +5,55 @@ exports.up = function (knex, Promise) {
     .createTable('agency_entry', function(table) {
       table.bigincrements('id').primary();
       table.timestamps();
-      table.string('agency_key').notNullable().unique();
+      table.string('agency_key')
+        .notNullable().unique();
     })
     
     .createTable('agency', function(table) {
       table.bigincrements('id').primary();
-      table.biginteger('agency_entry_id').unsigned().notNullable().references('id').inTable('agency_entry').onDelete('CASCADE').index();
+      table.biginteger('agency_entry_id').unsigned().notNullable()
+        .references('id').inTable('agency_entry').onDelete('CASCADE')
+        .index();
       table.timestamp('created_at').defaultTo(knex.fn.now());
       table.timestamp('updated_at').defaultTo(knex.fn.now());
 
-      table.string('agency_id').notNullable();
-      table.string('agency_name').notNullable();
-      table.string('agency_url').notNullable();
-      table.string('agency_timezone').notNullable();
+      table.string('agency_id')
+        .notNullable();
+      table.string('agency_name')
+        .notNullable();
+      table.string('agency_url')
+        .notNullable();
+      table.string('agency_timezone')
+        .notNullable();
       table.string('agency_lang');
       table.string('agency_phone');
       table.string('agency_fare_url');
       table.timestamp('last_update');
+      
+      // agency_id is dataset unique.
       table.unique(['agency_entry_id', 'agency_id']);
     })
     
     .createTable('stop', function(table) {
       table.bigincrements('id').primary();
-      table.biginteger('agency_entry_id').unsigned().notNullable().references('id').inTable('agency_entry').onDelete('CASCADE').index();
+      table.biginteger('agency_entry_id')
+        .unsigned().notNullable()
+        .references('id').inTable('agency_entry').onDelete('CASCADE')
+        .index();
       table.timestamp('created_at').defaultTo(knex.fn.now());
       table.timestamp('updated_at').defaultTo(knex.fn.now());
                         
-      table.string('stop_id').notNullable().index();
+      table.string('stop_id')
+        .notNullable()
+        .index();
       table.string('stop_code');
-      table.string('stop_name').notNullable();
+      table.string('stop_name')
+        .notNullable();
       table.string('stop_desc');
-      table.decimal('stop_lat', 16, 13).notNullable();
-      table.decimal('stop_lon', 16, 13).notNullable();
+      table.decimal('stop_lat', 16, 13)
+        .notNullable();
+      table.decimal('stop_lon', 16, 13)
+        .notNullable();
       table.string('zone_id');
       table.string('stop_url');
 
@@ -52,32 +69,56 @@ exports.up = function (knex, Promise) {
       //table.string('stop_region');
       //table.string('stop_postcode');
       //table.string('stop_country');
+      
+      // stop_id is dataset unique.
+      table.unique(['agency_entry_id', 'stop_id']);
     })
 
     .createTable('route', function(table) {
       table.bigincrements('id').primary();
-      table.biginteger('agency_entry_id').unsigned().notNullable().references('id').inTable('agency_entry').onDelete('CASCADE').index();
+      table.biginteger('agency_entry_id')
+        .unsigned().notNullable()
+        .references('id').inTable('agency_entry').onDelete('CASCADE')
+        .index();
       table.timestamp('created_at').defaultTo(knex.fn.now());
       table.timestamp('updated_at').defaultTo(knex.fn.now());
 
-      table.string('route_id').index();
-      table.biginteger('agency_id').unsigned().references('id').inTable('agency').onDelete('CASCADE').index();
-      table.string('route_short_name').notNullable();
-      table.string('route_long_name').notNullable();
+      table.string('route_id')
+        .index();
+      table.biginteger('agency_id')
+        .unsigned()
+        .references('id').inTable('agency').onDelete('CASCADE')
+        .index();
+      table.string('route_short_name')
+        .notNullable();
+      table.string('route_long_name')
+        .notNullable();
       table.string('route_desc');
-      table.integer('route_type').notNullable();
+      table.integer('route_type')
+        .notNullable();
       table.string('route_url');
       table.string('route_color');
       table.string('route_text_color');
+      
+      // route_id is dataset unique.
+      table.unique(['agency_entry_id', 'route_id']);
     })
     
     .createTable('service', function(table) {
       table.bigincrements('id').primary();
-      table.biginteger('agency_entry_id').unsigned().notNullable().references('id').inTable('agency_entry').onDelete('CASCADE').index();
+      table.biginteger('agency_entry_id')
+        .unsigned().notNullable()
+        .references('id').inTable('agency_entry').onDelete('CASCADE')
+        .index();
       table.timestamp('created_at').defaultTo(knex.fn.now());
       table.timestamp('updated_at').defaultTo(knex.fn.now());
 
-      table.string('service_id').notNullable().index();
+      table.string('service_id')
+        .notNullable()
+        .index();
+
+        // service_id is dataset unique.
+        table.unique(['agency_entry_id', 'service_id']);
     })
     
     .createTable('calendar', function(table) {
@@ -85,14 +126,24 @@ exports.up = function (knex, Promise) {
       table.timestamp('created_at').defaultTo(knex.fn.now());
       table.timestamp('updated_at').defaultTo(knex.fn.now());
 
-      table.biginteger('service_id').unsigned().notNullable().references('id').inTable('service').onDelete('CASCADE').index();
-      table.integer('monday').notNullable();
-      table.integer('tuesday').notNullable();
-      table.integer('wednesday').notNullable();
-      table.integer('thursday').notNullable();
-      table.integer('friday').notNullable();
-      table.integer('saturday').notNullable();
-      table.integer('sunday').notNullable();
+      table.biginteger('service_id')
+        .unsigned().notNullable()
+        .references('id').inTable('service').onDelete('CASCADE')
+        .index();
+      table.integer('monday')
+        .notNullable();
+      table.integer('tuesday')
+        .notNullable();
+      table.integer('wednesday')
+        .notNullable();
+      table.integer('thursday')
+        .notNullable();
+      table.integer('friday')
+        .notNullable();
+      table.integer('saturday')
+        .notNullable();
+      table.integer('sunday')
+        .notNullable();
       table.timestamp('start_date')
         .notNullable();
       table.timestamp('end_date')
@@ -104,9 +155,14 @@ exports.up = function (knex, Promise) {
       table.timestamp('created_at').defaultTo(knex.fn.now());
       table.timestamp('updated_at').defaultTo(knex.fn.now());
 
-      table.biginteger('service_id').unsigned().notNullable().references('id').inTable('service').onDelete('CASCADE').index();
-      table.integer('exception_type').notNullable();
-      table.timestamp('date').notNullable();
+      table.biginteger('service_id')
+        .unsigned().notNullable()
+        .references('id').inTable('service').onDelete('CASCADE')
+        .index();
+      table.timestamp('date')
+        .notNullable();
+      table.integer('exception_type')
+        .notNullable();
     })
 
     .createTable('shape', function(table) {
@@ -114,7 +170,8 @@ exports.up = function (knex, Promise) {
       table.timestamp('created_at').defaultTo(knex.fn.now());
       table.timestamp('updated_at').defaultTo(knex.fn.now());
       
-      table.string('shape_id').notNullable();
+      table.string('shape_id')
+        .notNullable();
     })
 
     .createTable('shape_sequence', function(table) {
@@ -122,10 +179,16 @@ exports.up = function (knex, Promise) {
       table.timestamp('created_at').defaultTo(knex.fn.now());
       table.timestamp('updated_at').defaultTo(knex.fn.now());
 
-      table.biginteger('shape_id').unsigned().notNullable().references('id').inTable('shape').onDelete('CASCADE').index();
-      table.decimal('shape_pt_lat', 16, 13).notNullable();
-      table.decimal('shape_pt_lon', 16, 13).notNullable();
-      table.integer('shape_pt_sequence').notNullable();
+      table.biginteger('shape_id')
+        .unsigned().notNullable()
+        .references('id').inTable('shape').onDelete('CASCADE')
+        .index();
+      table.decimal('shape_pt_lat', 16, 13)
+        .notNullable();
+      table.decimal('shape_pt_lon', 16, 13)
+        .notNullable();
+      table.integer('shape_pt_sequence')
+        .notNullable();
       table.decimal('shape_dist_traveled', 24, 11);
     })
     
@@ -135,18 +198,35 @@ exports.up = function (knex, Promise) {
       table.timestamp('updated_at').defaultTo(knex.fn.now());
 
       // Keep reference to the agency entry for easy removal.
-      table.biginteger('agency_entry_id').unsigned().notNullable().references('id').inTable('agency_entry').onDelete('CASCADE').index();
+      table.biginteger('agency_entry_id')
+        .unsigned().notNullable()
+        .references('id').inTable('agency_entry').onDelete('CASCADE')
+        .index();
 
-      table.string('trip_id').notNullable().index();
-      table.biginteger('route_id').unsigned().notNullable().references('id').inTable('route').onDelete('CASCADE').index();
-      table.biginteger('service_id').unsigned().notNullable().references('id').inTable('service').onDelete('CASCADE').index();
+      table.string('trip_id')
+        .notNullable()
+        .index();
+      table.biginteger('route_id')
+        .unsigned().notNullable()
+        .references('id').inTable('route').onDelete('CASCADE')
+        .index();
+      table.biginteger('service_id')
+        .unsigned().notNullable()
+        .references('id').inTable('service').onDelete('CASCADE')
+        .index();
       table.string('trip_headsign');
       table.string('trip_short_name');
       table.integer('direction_id');
       table.string('block_id');
-      table.biginteger('shape_id').unsigned().references('id').inTable('shape').onDelete('CASCADE').index();
+      table.biginteger('shape_id')
+        .unsigned()
+        .references('id').inTable('shape').onDelete('CASCADE')
+        .index();
       table.integer('wheelchair_accessible');
       table.integer('bikes_allowed');
+
+      // trip_id is dataset unique.
+      table.unique(['agency_entry_id', 'trip_id']);
     })
 
     .createTable('stop_time', function(table) {
@@ -155,15 +235,25 @@ exports.up = function (knex, Promise) {
       table.timestamp('updated_at').defaultTo(knex.fn.now());
 
       // Keep reference to the agency entry for easy removal.
-      table.biginteger('agency_entry_id').unsigned().notNullable().references('id').inTable('agency_entry').onDelete('CASCADE').index();
-      
-      table.biginteger('trip_id').unsigned().notNullable().references('id').inTable('trip').onDelete('CASCADE').index();
       table.string('arrival_time');
       table.string('departure_time');
-      table.biginteger('stop_id').unsigned().notNullable().references('id').inTable('stop').onDelete('CASCADE').index();
+      table.biginteger('agency_entry_id')
+        .unsigned().notNullable()
+        .references('id').inTable('agency_entry').onDelete('CASCADE')
+        .index();
+      
+      table.biginteger('trip_id')
+        .unsigned().notNullable()
+        .references('id').inTable('trip').onDelete('CASCADE')
+        .index();
+      table.biginteger('stop_id')
+        .unsigned().notNullable()
+        .references('id').inTable('stop').onDelete('CASCADE')
+        .index();
       table.integer('stop_sequence');
       table.string('stop_headsign');
       table.integer('pickup_type');
+      table.integer('drop_off_time');  // In seconds, unofficial (?)
       table.integer('drop_off_type');
       table.decimal('shape_dist_traveled', 24, 11);
       table.integer('timepoint');
